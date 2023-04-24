2cea4d060e626f126f3b47f00995d3e22622096e
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-04-24 15:19:54 -0400

    typeDef and resolvers built

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,12 +5,11 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // We split the token string into an array and return actual token
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
